67b82c0cacf55f7b9f96162cbbeb02e37afa49ab
Sina Siddiqi <sinas@andrew.cmu.edu>
2017-06-02 13:26:14 -0400

    Working on role transition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def home
     if logged_in?
-      if current_user.is_care_deacon?
+      if current_user.role?('care deacon')
         @cases_user = Case.for_deacon(current_user.id).chronological.paginate(page: params[:page]).per_page(10)
       else
         @need_review = Case.submitted.paginate(page: params[:page]).per_page(10)
--- test/sets/user_contexts.rb
+++ test/sets/user_contexts.rb
@@ -2,10 +2,10 @@ module Contexts
   module UserContexts
     def create_users
       @jason = FactoryGirl.create(:user)
-      @jon = FactoryGirl.create(:user, first_name: "Jon", last_name: "Wu", email: "jonWu@gmail.com", password_digest: "meh", phone: "4121112222", role: "deacon", is_care_deacon: true)
-      @larry = FactoryGirl.create(:user, first_name: "Larry", last_name: "Heimann", email: "LarryH@gmail.com", password_digest: "meh", phone: "4123334444", role: "deacon", is_care_deacon: false)
-      @paula = FactoryGirl.create(:user, first_name: "Paula", last_name: "Troy", email: "PaulaT@gmail.com", password_digest: "meh", phone: "4125556666", role: "deacon", is_care_deacon: false, active: false)
-      @connector = FactoryGirl.create(:user, first_name: "Apple", last_name: "Seed", email: "connect@gmail.com", password_digest: "meh", phone: "4121112222", role: "care_connector", is_care_deacon: false)
+      @jon = FactoryGirl.create(:user, first_name: "Jon", last_name: "Wu", email: "jonWu@gmail.com", password_digest: "meh", phone: "4121112222", role: "deacon")
+      @larry = FactoryGirl.create(:user, first_name: "Larry", last_name: "Heimann", email: "LarryH@gmail.com", password_digest: "meh", phone: "4123334444", role: "deacon")
+      @paula = FactoryGirl.create(:user, first_name: "Paula", last_name: "Troy", email: "PaulaT@gmail.com", password_digest: "meh", phone: "4125556666", role: "deacon", active: false)
+      @connector = FactoryGirl.create(:user, first_name: "Apple", last_name: "Seed", email: "connect@gmail.com", password_digest: "meh", phone: "4121112222", role: "staff")
 
     end
 
