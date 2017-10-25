6cf105f14e098592fe4fee50a120f5a0c7a5c998
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-10-25 19:01:31 -0400

    timeline created, cleanup needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(first_name: 'Heidi', last_name: 'Dekkon', email: 'head_deacon@gmail.com', password: 'meh', phone: '9999999999', role: 'head_deacon')
-User.create(first_name: 'Jason', last_name: 'Chen', email: 'jason95014@gmail.com', password: 'meh', phone: '9999999999', role: 'admin')
-User.create(first_name: 'Johnny', last_name: 'Wu', email: 'jwu@gmail.com', password: 'meh', phone: '9999999999', role: 'care_deacon')
-User.create(first_name: 'Justin', last_name: 'Chen', email: 'jchen@gmail.com', password: 'meh', phone: '9999999999', role: 'financial_deacon')
-User.create(first_name: 'Kane', last_name: 'Manage', email: 'staff@gmail.com', password: 'meh', phone: '9999999999', role: 'staff')
+User.create(first_name: 'Heidi', last_name: 'Dekkon', email: 'head_deacon@gmail.com', password: 'meh', phone: '9733074206', role: 'head_deacon')
+User.create(first_name: 'Jason', last_name: 'Chen', email: 'jason95014@gmail.com', password: 'meh', phone: '9733074206', role: 'admin')
+User.create(first_name: 'Johnny', last_name: 'Wu', email: 'jwu@gmail.com', password: 'meh', phone: '9733074206', role: 'care_deacon')
+User.create(first_name: 'Justin', last_name: 'Chen', email: 'jchen@gmail.com', password: 'meh', phone: '9733074206', role: 'financial_deacon')
+User.create(first_name: 'Kane', last_name: 'Manage', email: 'staff@gmail.com', password: 'meh', phone: '9733074206', role: 'staff')
 
 
 6.times do
   Case.create({
     client_name: Faker::Name.name,
     client_first_name: Faker::Name.name,
-    date_submitted: Faker::Date,
+    date_submitted: Faker::Date.between(1.year.ago, Date.today),
     summary: Faker::Lorem.paragraph(7),
     notes: Faker::Lorem.sentence(3),
     recommendation: Faker::Lorem.sentence(1),
