cdf5a2ca2ac46a44d31b790bb93a20a582b88dc8
profh <profh@cmu.edu>
2017-11-09 12:26:19 -0500

    Added a cap deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,55 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+# require 'puma/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+server 'acacmanna.org', :web, :app, :db, primary: true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, 'manna'
+set :user, 'stephen'
+set :group, 'stephen'
+set :deploy_to, "/home/#{user}/apps/#{application}"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :scm, 'git'
+set :git_enable_submodules, 1
+set :deploy_via, :remote_cache
+set :repository, "git@github.com:cmu-is-projects/Manna.git"
+set :branch, 'master'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :use_sudo, false
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
+# whenever configuration (for cron jobs)
+# require 'whenever/capistrano'
+# set :whenever_command, 'bundle exec whenever'
+
+# share public/uploads
+set :shared_children, shared_children + %w{public/uploads}
+
+# allow password prompt
+default_run_options[:pty] = true
+
+# turn on key forwarding
+ssh_options[:forward_agent] = true
+
+# keep only the last 5 releases
+after 'deploy', 'deploy:cleanup'
+after 'deploy:restart', 'deploy:cleanup'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :symlink_shared do
+    run "ln -s /home/stephen/apps/manna/shared/settings.yml /home/stephen/apps/manna/releases/#{release_name}/config/"
+  end
+end
+
+before "deploy:assets:precompile", "deploy:symlink_shared"
+
+# before "deploy:restart", "deploy:symlink_shared"
+
+# task :symlink_config, roles: :app do
+#   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#   run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
 # end
+# after "deploy:finalize_update", "deploy:symlink_config"
